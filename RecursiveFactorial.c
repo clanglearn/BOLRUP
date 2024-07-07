0cd31ad02c30ebb9b0b1f4af873c0dc235db3868
BOLRUP <ksc050210@gmail.com>
2024-07-08 03:17:47 +0900

    refactor : Factorial 함수의 원형 선언 추가

--- RecursiveFactorial.c
+++ RecursiveFactorial.c
@@ -1,6 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h> 
 
+int Factorial(int num);
+
+int main(void) {
+	int n;
+	scanf("%d", &n);
+	printf("%d\n", Factorial(n));
+	return 0;
+}
+
 int Factorial(int num) {
 	if (num == 0) {
 		return 1;
@@ -11,11 +20,4 @@ int Factorial(int num) {
 	else {
 		return Factorial(num - 1) * num;
 	}
-}
-
-int main(void) {
-	int n;
-	scanf("%d", &n);
-	printf("%d\n", Factorial(n));
-	return 0;
 }
